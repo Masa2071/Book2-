b0f06227732832f86ff633a07eda0a0313f76bc8
EC2 Default User <ec2-user@ip-172-31-45-58.ec2.internal>
2020-11-13 13:27:00 +0000

    [add]usershow.userindex

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,16 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.all
+    @book = Book.new
+    @user =  current_user
+  end
 
   def show
     @user = User.find(params[:id])
+    @user_book = @user.books
     @book = Book.new #空のインスタンス生成
   end
-  
+
   def book_params
     params.require(:book).permit(:title, :body)
   end
