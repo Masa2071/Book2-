c498b3ecb0ebbad1be771b171a5c0d51037deb98
EC2 Default User <ec2-user@ip-172-31-45-58.ec2.internal>
2020-11-14 07:50:57 +0000

    編集

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,17 @@ class UsersController < ApplicationController
     @book = Book.new #空のインスタンス生成
   end
 
-  def book_params
-    params.require(:book).permit(:title, :body)
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id]) 
+    @user.update(user_params)
+    redirect_to user_path()
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :introduction)
   end
 end
